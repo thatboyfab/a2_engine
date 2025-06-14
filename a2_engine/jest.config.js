f32fb5665d26acb13bf3aa2cc610b458fb12a22b
leo-zoho <leoatzoho@gmail.com>
2025-06-14 08:50:08 +0000

    update

---
+++ a2_engine/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  testMatch: [
+    "<rootDir>/services/**/tests/**/*.js",
+    "<rootDir>/libs/**/tests/**/*.js"
+  ],
+  testEnvironment: "node",
+  verbose: true,
+  detectOpenHandles: true
+};
